c75bff8100abe4b9bbe628516a4162977f3b0495
Andrey Lunev <lnvbaby@yandex.ru>
2024-12-17 10:46:03 +0300

    Sazhik

--- 4.py
+++ 4.py
@@ -1,13 +1,1 @@
-from datetime import *
-
-try:
-    print('Введите дату отправления в виде ДД/ММ/ГГ')
-    data = datetime.strptime(input(), '%d/%m/%Y')
-except:
-    exit()
-data_days = data + timedelta(10000, 0)
-data_mins = data + timedelta(seconds=60000000)
-data_secs = data + timedelta(seconds=1000000000)
-print(data_days)
-print(data_mins)
-print(data_secs)
+Сажин крутой
